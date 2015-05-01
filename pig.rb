b9da5417c83910652412388fccd6646a97fa6fc4
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-04-30 19:44:26 -0700

    done

---
+++ pig.rb
@@ -0,0 +1,37 @@
+def pig_sentence(sentence)
+	new_sentence = []
+	sentence = sentence.split(" ")
+	sentence.each do |word|
+		new_sentence << pig_word(word)
+	end
+	new_sentence.join(" ")
+end
+
+def pig_word(word)
+	vowels = ["a","e","i","o","u"]
+	i = 0
+	while(i < word.length)
+		if vowels.include?(word[0])
+		 	return word + "ay"
+		elsif word[0] == "q"
+			return word[2..word.length] + word[0..1] + "ay" 
+		else
+			word = word[1..word.length] + word[0]
+		end	
+		i += 1
+	
+	end
+
+	
+end		
+
+
+p pig_sentence("apples and bananas")
+
+p pig_sentence("duck")
+ p pig_sentence("squeak")
+
+p pig_sentence("i am a junior developer")
+p pig_sentence("run quickly")
+
+
